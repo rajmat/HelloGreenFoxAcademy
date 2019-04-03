8185e169e18fef1b6f1a58d3c17ee0889e312c6e
rajmat <rajmund.matus@gmail.com>
2019-04-03 16:18:45 +0200

    SimpleGoldenAcornApp has been done without prop-types yet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function Button(props) {
+  return (
+    <button onClick={props.onClick}>{props.name}</button>
+  )
+};
+
+function Display(props) {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+};
+
+class GoldenAcornApp extends React.Component {
+  constructor() {
+    super();
+    this.state = { number: 0 };
+    this.buyOne = this.buyOne.bind(this);
+    this.eatOne = this.eatOne.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  buyOne() {
+    this.setState(previousState => ({
+      number: previousState.number + 1
+    }), () => { console.log(this.state) });
+  }
+
+  eatOne() {
+    this.setState({
+      number: Math.max(this.state.number - 1, 0)
+    }, () => { console.log(this.state) });
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {
+    if (event.keyCode == 38) {
+      this.buyOne();
+    } else if (event.keyCode === 40) {
+      this.eatOne();
+    }
+  };
+
+  render() {
+    return (
+      <div className="content"><br/>
+        <Button onClick={this.buyOne} name="Buy one" ></Button><br/><br/>
+        <Display >{this.state.number}</Display><br/>
+        <Button onClick={this.eatOne} name="Eat one"></Button><br/>
+      </div>
+    )
+  }
+};
+
 ReactDOM.render(
-  <h1>Hello, Green Fox Academy!</h1>,
-  document.getElementById('root'),
+  <GoldenAcornApp />,
+  document.getElementById('root')
 );
